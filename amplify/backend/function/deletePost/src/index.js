ed39b9fb492702192d3a9b98fc2cef64ffbc55c5
Gregmar Cruz D.O <49351487+GregmarC@users.noreply.github.com>
2023-04-08 09:52:23 -0400

    connect to db for addPost

--- amplify/backend/function/deletePost/src/index.js
+++ amplify/backend/function/deletePost/src/index.js
@@ -1,25 +1,17 @@
+
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
- 
-const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const params = {
-  TableName : 'Post',
-  /* Item properties will depend on your application concerns */
-  Item: {
-     id: 2,
-     postMsg: 'Second lambda'
-  }
-}
-
 exports.handler = async (event) => {
-  try {
-    await docClient.put(params).promise();
-    return { body: 'Successfully created item!' }
-  } catch (err) {
-    
-    return { error: err }
-  }
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
 };
